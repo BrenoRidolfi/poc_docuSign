104697a0426e9f9397fb4b580fdc0324cbba2c17
Breno Ridolfi Loures Franco <brfranco@latam.stefanini.com>
2021-09-02 09:25:28 -0300

    Start

---
+++ launcher-csharp/eSignature/Models/LoadPageDocuSign.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocuSign.CodeExamples.eSignature.Models
+{
+    public class LoadPageDocuSign
+    {
+        public string Link { get; set; }
+    }
+}
